8a7735187b90a1faa60faacda7169ef4fae24c04
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-09 11:42:15 -0700

    Fixed Fetch Error

--- logic.js
+++ logic.js
@@ -17,20 +17,4 @@ fetch('rapidapi.com', options)
 }) 
 .then(function (data) {
     console.log(data)
-    for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
-
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
-
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableBody.appendChild(createTableRow);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
     });
