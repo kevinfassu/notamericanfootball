bc3468a6573eb08b1fff62b47cda912f68b65e44
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-10 14:43:57 -0700

    Added fetch standings

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,18 @@
- twitterfeed
-
-
-
- main
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7c1d331df8msh9322aee26b0f534p1d832bjsn27c5f09f0a59',
-		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api-football-v1.p.rapidapi.com/v3/standings?season=2021&league=39",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "7c1d331df8msh9322aee26b0f534p1d832bjsn27c5f09f0a59",
+		"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 	}
 };
 
-fetch('https://api-football-v1.p.rapidapi.com/v3/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err))
-	.then(function (response) {
-		return response.json()
-	})
-	.then(function (data) {
-		console.log(data)
-	});
+$.ajax(settings).done(function (response) {
+	var standingsPremier =
+	console.log(response);
+});
 
 
 const twtOptions = {
@@ -29,6 +21,7 @@ const twtOptions = {
 		'Authorization': 'Bearer' + 'AAAAAAAAAAAAAAAAAAAAAH01dgEAAAAADrG3yG8hXBXxOYFCAuTnI2RrB74%3DfrbGgECz6VCpDY40FDEqtaonGivL50BO9lL955EfqkDhSkbmw1'
 	}
 }
+
 //need to grab cors proxy again
 fetch('https://cors-anywhere.herokuapp.com/corsdemo/https://api.twitter.com/2/', twtOptions)
 	.then(response => response.json())
