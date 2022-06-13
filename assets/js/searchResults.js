64a2f10a8ddc5bd01915c9ffe1a8ac6fade34bd6
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-12 18:13:32 -0700

    fixed search rendering styling

--- assets/js/searchResults.js
+++ assets/js/searchResults.js
@@ -43,26 +43,26 @@ function searchApi(searchType, searchInput) {
 function renderResults(resultObj) {
     console.log(resultObj);
     if ("team" in resultObj) {
-        var resultBody = $("<div></div").attr("class", "flex ").appendTo("#search-content");
-        var resultsCard = $("<div></div>").attr("class", "inline-block p-4 max-w-md bg-stone-300 border-gray-200 shadow-sm hover:bg-stone-500").appendTo(resultBody);
-        var cardTitleEl = $("<h3></h3>").attr("class", "mb-3 text-lg font-semibold text-gray-900")
+        var resultBody = $("<div></div").attr("class", "card").appendTo("#search-content");
+        var resultsCard = $("<div></div>").appendTo(resultBody);
+        var cardTitleEl = $("<h3></h3>")
 
         cardTitleEl.text(resultObj.team.name);
-        var logoEl = $("<img></img").attr("src", resultObj.team.logo)
+        var logoEl = $("<img></img").attr({src: resultObj.team.logo, "class": "object-scale-down"})
 
-        var countryEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var countryEl = $("<p></p>")
         countryEl.text(resultObj.team.country);
        
-        var dateEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var dateEl = $("<p></p>")
         dateEl.text(resultObj.team.founded);
         
-        var venueTitleEl = $("<h4></h4>").attr("class", "mb-3 text-lg font-semibold text-gray-900")
+        var venueTitleEl = $("<h4></h4>")
         venueTitleEl.text(resultObj.venue.name);
        
-        var cityNameEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var cityNameEl = $("<p></p>")
         cityNameEl.text(resultObj.venue.city);
        
-        var capacitySizeEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var capacitySizeEl = $("<p></p>")
         capacitySizeEl.text(resultObj.venue.capacity);
 
         resultBody.append(cardTitleEl, logoEl, countryEl, dateEl, venueTitleEl, cityNameEl, capacitySizeEl);
@@ -70,14 +70,14 @@ function renderResults(resultObj) {
 
     } else if ("league" in resultObj) {
         var resultBody = $("<div></div").attr("class", "flex").appendTo("#search-content");
-        var resultsCard = $("<div></div>").attr("class", "block p-4 max-w-md bg-stone-300 border-gray-200 shadow-sm hover:bg-stone-500").appendTo(resultBody);
+        var resultsCard = $("<div></div>").attr("class", "card").appendTo(resultBody);
 
-        var leagueNameEl = $("<h4></h4>").attr("class", "mb-3 text-lg font-semibold text-gray-900")
+        var leagueNameEl = $("<h4></h4>").attr("class", "card")
         leagueNameEl.text(resultObj.league.name);
        
         var leagueLogo = ("<img></img").attr("src", resultObj.league.logo);
 
-        var countryEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var countryEl = $("<p></p>").attr("class", "card");
         countryEl.text(resultObj.country.name)
        
         var countryFlag = ("<img></img").attr("src", resultObj.country.flag);
@@ -90,27 +90,27 @@ function renderResults(resultObj) {
 
     } else if ("player" in resultObj) {
         var resultBody = $("<div></div").attr("class", "flex").appendTo("#search-content");
-        var resultsCard = $("<div></div>").attr("class", "block p-4 max-w-md bg-stone-300 border-gray-200 shadow-sm hover:bg-stone-500").appendTo(resultBody);
+        var resultsCard = $("<div></div>").attr("class", "card").appendTo(resultBody);
 
-        var playerNameEl = $("<h3></h3>").attr("class", "mb-3 text-lg font-semibold text-gray-900");
+        var playerNameEl = $("<h3></h3>").attr("class", "card");
         playerNameEl.text(resultObj.player.firstname + resultObj.player.lastname);
        
-        var playerAgeEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var playerAgeEl = $("<p></p>").attr("class", "card");
         playerAgeEl.text(resultObj.player.age)
         
-        var playerNationalityEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var playerNationalityEl = $("<p></p>").attr("class", "card");
         playerNationalityEl.text(resultObj.player.nationality);
        
-        var playerHieghtEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var playerHieghtEl = $("<p></p>").attr("class", "card");
         playerHieghtEl.text(resultObj.player.height);
         
-        var playerWeightEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var playerWeightEl = $("<p></p>").attr("class", "card");
         playerWeightEl.text(resultObj.player.weight);
 
         if (!resultObj.player.injured) {
-            var playerInjuries = $("<p>Player is currently: uninjured</p>").attr("class", "font-normal, mb-1, text-stone-500");
+            var playerInjuries = $("<p>Player is currently: uninjured</p>").attr("class", "card");
         } else {
-            var playerInjuries = $("<p>Player is currently: injured</p>").attr("class", "font-normal, mb-1, text-stone-500");
+            var playerInjuries = $("<p>Player is currently: injured</p>").attr("class", "card");
         }
         var playerImgEl = $("<img></img").attr("src", resultObj.player.photo);
 
@@ -120,12 +120,12 @@ function renderResults(resultObj) {
 
     else {
         var resultBody = $("<div></div").attr("class", "flex").appendTo("#search-content");
-        var resultsCard = $("<div></div>").attr("class", "block p-4 max-w-md bg-stone-300 border-gray-200 shadow-sm hover:bg-stone-500").appendTo(resultBody);
+        var resultsCard = $("<div></div>").attr("class", "card").appendTo(resultBody);
       
-        var countryName = $("<h3></h3>").attr("class", "mb-3 text-lg font-semibold text-gray-900")
+        var countryName = $("<h3></h3>").attr("class", "card")
         countryName.text(resultObj.name);
 
-        var abbrevEl = $("<p></p>").attr("class", "font-normal, mb-1, text-stone-500");
+        var abbrevEl = $("<p></p>").attr("class", "card");
         abbrevEl.text(resultObj.code);
 
         var flagEl = $("<img></img").attr("src", resultObj.flag);
